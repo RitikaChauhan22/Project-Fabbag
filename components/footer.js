d188eb7e467251a507ef463d49bbd4cbd133b3ff
RitikaChauhan22 <91660344+RitikaChauhan22@users.noreply.github.com>
2021-12-17 20:05:41 +0530

    first commit

---
+++ components/footer.js
@@ -0,0 +1,81 @@
+function footer() {
+
+return `<footer class="footer-cont" style="padding: 80px 0px 35px 0px;">
+<div id="cont">
+    <div class="row">
+       <div id="footer-top">
+           <p id="title">INFORMATION</p>
+           <br>
+           <div>
+           <ul>
+               <li style=" margin-top: -20px;">
+                   About Us
+               </li>
+               <li>
+                   Associated Brands
+               </li>
+               <li>
+                   FAQ
+               </li>
+               <li>
+                   Collabrate with Fab Bag
+               </li>
+               <li>
+                   Contact Us
+              </li>
+              <li>
+                  Returns & Refunds
+              </li>
+              <li>
+                  Terms and Conditions
+              </li>
+              <li>
+                  Privacy Policy
+              </li>
+           </ul>
+          </div>
+       </div> 
+       <div id= "mid-foot">
+            <p id="title">
+                CUSTOMER CARE
+            </p> 
+            <div>
+              <p id="info">Call : 022-6805-6010</p> 
+              <p id="info">(Monday to Friday : 9 AM - 7 PM</p>
+              <br>
+              
+              <p id="info">Email : crew@fabbag.com</p>
+            </div>
+      </div>
+      <div id="news">
+          <p id="title">
+              NEWSLETTER
+          </p>
+          <div id="subtitle">
+             <p id="info">Signup for the latest news, updates and offers</p>
+             <br>
+             <div id="email-sign">
+                 <div>
+                 <input id="input1" type="text" placeholder="Your email address">
+                 </div>
+                 <div>
+                 <button class="btn">SUBSCRIBE</button>
+                 </div>
+              </div>
+              <br>
+              <p id="info" style="margin-bottom: 5px;
+              font-size: 14px;
+              letter-spacing: 0.5px;
+              line-height: 30px;">
+                  Don't worry we don't spam
+              </p>
+          </div>
+      </div>
+    </div>
+</div>
+</footer>
+<p id="info" style="text-align: center;">Copyright © 2021 fabbag.com All rights reserved.</p>`
+
+}
+
+export {footer}
---
+++ components/navbar.js
@@ -0,0 +1,34 @@
+function navbar() {
+
+return `<div id="bar">
+<div id="nav-head">
+<p>AVAILABLE NOW - THE X-MUST HAVES DECEMBER FAB BAG!</p>
+</div>
+<div id="nav">
+<div id="subs"><a href="cart.html">SUBSCRIBE NOW</a></div>
+<div id="mid"><a href="home.html"><img id="logo" src="https://cdn.shopify.com/s/files/1/0052/7551/6995/files/FABBAG-LOGO_96ac9306-4c81-4ab8-8d2b-4e818eed7f76_2048x.png?v=1606482289" alt=""></a>
+</div>
+<div id="right-nav">
+    <div id="search-icon">
+    <a href="search.html">
+    <img id="srch" src= "https://icons.iconarchive.com/icons/praveen/minimal-outline/128/search-icon.png"/>
+    </a>
+    </div>
+    
+    <div id="profile">
+        <a href="fabsign.html"><img id="pro" src="https://cdn-icons-png.flaticon.com/512/848/848043.png"/></a>
+    </div>
+    <div id="wishlist">
+        <a href="wish.html"><img id="wish" src="https://cdn-icons-png.flaticon.com/128/6320/6320703.png"/></a>
+       
+    </div id="cart">
+    <div id="car">
+        <a href="cart.html"><img id="car" src="https://tse4.mm.bing.net/th?id=OIP.fYPFuy5ypGwIotPEJMp_BAHaHa&pid=Api&P=0&w=300&h=300"/></a>
+    </div>
+</div>
+</div>
+</div>`
+
+}
+
+export {navbar}
